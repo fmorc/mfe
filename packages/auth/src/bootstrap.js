5ac88e972dc3881b96e3a3bbb9df3725fe2523ba
Fernando <fernando.mor.c@gmail.com>
2025-11-14 21:18:29 -0600

    added auth subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
     const history = defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
 
     if (onNavigate) {
@@ -11,7 +11,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     }
 
     ReactDOM.render(
-        <App history={history} />,
+        <App onSignIn={onSignIn} history={history} />,
         el
     )
 
@@ -26,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 }
 
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
     if (devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
